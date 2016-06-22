a52a25b851e6f62813ffadac8db0cbbbb4ad540f
robotcake <cathywong@smrmh.com>
2016-06-22 20:37:35 +0800

    json

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,76 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 
+ /*$("#main").append("Cathy Wong");
+ */
+//  var formattedName = HTMLheaderName.replace("%data%","Cathy Wong");
+//  var formattedRole = HTMLheaderRole.replace("%data%","Director");
+ 
+//   $("#header").prepend(formattedRole);
+//  $("#header").prepend(formattedName);
+
+ 
+// var skills = ["html","css","javascript"];
+// var bio = {
+// 	"name" :"Cathy Wong",
+// 	"age" : 31,
+// 	"skills" : skills
+// };
+
+// var work = {};
+// work.position ="Front End Developer";
+// work.employer ="South Mountain RMH Company Limited";
+// work.year = "4";
+
+// var education ={};
+// education["name"]="City University of Hong Kong";
+// education["years"]="2004-2008";
+// education["city"]="Hong Kong";
+// $("#main").append(bio.name);
+// $("#main").append(work["position"]);
+// $("#main").append(education.name);
+
+// var education{
+// 	"schools":[{
+// 		"city":"Hong Kong",
+// 		"major":"Creative Media",
+// 		"gradyear":"2008"
+
+// 	},
+// 	{ "city":"Hong Kong",
+// 		"major":"AAMET",
+// 		"gradyear":"2006"
+
+// 	}
+// 	],
+// 	"onlineCourse":[
+// 	{
+// 		"title":"2008"
+// 	}]
+// }
+
+var work{
+	"jobs":[{
+		"employer":"South Mountain RMH Company Limited",
+		"title":"",
+		"location":"",
+		"dates":"",
+		"description":""
+	},
+	{
+		"employer":"",
+		"title":"",
+		"location":"",
+		"dates":"",
+		"description":""
+	},
+	{
+		"employer":"",
+		"title":"",
+		"location":"",
+		"dates":"",
+		"description":""
+	}
+	]
+}
